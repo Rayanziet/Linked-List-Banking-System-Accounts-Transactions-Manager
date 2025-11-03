a090cb5cce991c3663f5b87201503318d9659ae8
Rayan <rayanzeitouny30@gmail.com>
2025-11-03 17:54:58 +0200

    Initial commit

---
+++ Data Structures Project/Data Structures Project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cstring>
+#include<string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
